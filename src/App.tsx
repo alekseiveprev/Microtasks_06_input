85f2f2abeb0e76058fec9e5813b927c3f9b83f0f
alekseiveprev <72622572+alekseiveprev@users.noreply.github.com>
2023-09-21 14:26:15 +0300

    feat: implement first button task

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import { FullInput } from './components/FullInput';
 
 import './style.css';
 
 export const App: FC<{ name: string }> = ({ name }) => {
+  // выполняет роль сервера
+  let [message, setMessage] = useState([
+    { message: 'message1' },
+    { message: 'message2' },
+    { message: 'message3' },
+  ]);
+
+  const addMessage = (title: string) => {
+    let newMessage = { message:title };
+    setMessage([ newMessage,...message,]);
+    // console.log(title);
+  };
+
   return (
-    <div>
-      <h1>Hello {name}!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className={'App'}>
+      {/* <div>
+        <input />
+        <button>+</button>
+      </div> */}
+      <FullInput addMessage={addMessage} />
+      {message.map((el, index) => {
+        return <div key={index}>{el.message}</div>;
+      })}
     </div>
   );
 };
